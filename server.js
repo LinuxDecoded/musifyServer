2e54e6aa1a1028de949a3a8dca795369f4f2d93e
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-07-03 14:01:17 +0530

    made requests asynchronous

--- server.js
+++ server.js
@@ -10,55 +10,49 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post("/refresh", (req, res) => {
+const spotifyApiOptions = {
+  redirectUri: process.env.REDIRECT_URI,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+}
+
+app.post("/refresh", async(req, res) => {
   const refreshToken = req.body.refreshToken
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
-  })
+  spotifyApiOptions.refreshToken = refreshToken
+  const spotifyApi = new SpotifyWebApi(spotifyApiOptions)
 
-  spotifyApi
-    .refreshAccessToken()
-    .then(data => {
-      res.json({
-        accessToken: data.body.access_token,
-        expiresIn: data.body.expires_in,
-      })
+  try {
+    const data = await spotifyApi.refreshAccessToken()
+    res.json({
+      accessToken: data.body.access_token,
+      expiresIn: data.body.expires_in,
     })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
+  }
 })
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:5173",
-    clientId: "27b1cecea7ef4b93a30575c8f5c997d6",
-    clientSecret: "5e1c9be5ad1a48e4a6f708e6bccb439a",
-  })
+  const spotifyApi = new SpotifyWebApi(spotifyApiOptions)
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      })
+  try {
+    const data = await spotifyApi.authorizationCodeGrant(code)
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
     })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
+  }
+  catch (err) {
+    console.log(err)
+    res.sendStatus(400)
+  }
 })
 
-app.get("/lyrics", async (req, res) => {
-  const lyrics =
-    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+app.post("/lyrics", async (req, res) => {
+  const lyrics = await lyricsFinder(req.body.artist, req.body.track) || "No Lyrics Found"
   res.json({ lyrics })
 })
 
